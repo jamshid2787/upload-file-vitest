c5fbcd3064751fe6d2392ae2b71da1214c4abcb1
Jamshidbek <macbook_uz@MacBook-Pro---Jamshidbek.local>
2024-04-20 14:33:29 +0500

    clean code bug fix

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Single from './components/singleUpload.vue';
+import Multiple from './components/multipleUpload.vue';
+</script>
+
+<template>
+	<Single />
+	<Multiple />
+</template>
+
+<style scoped></style>
